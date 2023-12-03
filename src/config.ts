24c695f4a9d82cad8975cb84797fea5d7f675137
DrWarpMan <drwarpman@gmail.com>
2023-12-03 16:54:33 +0100

    username regex

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const configSchema = z.array(z.object({
-    username: z.string().min(1),
+    username: z.string().min(1).regex(/^[a-zA-Z0-9_]+$/),
     password: z.string().min(1),
     games: z.array(z.number().int().positive()).min(1),
 }));
