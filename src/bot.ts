f3581e039e2f1807b97b4f46e520b9cb2e17c328
DrWarpMan <drwarpman@gmail.com>
2025-07-30 15:54:25 +0200

    increase timeout

--- src/bot.ts
+++ src/bot.ts
@@ -5,7 +5,8 @@ import type { TokenStorage } from "./token-storage";
 
 type LoginDetails = Parameters<Steam["logOn"]>[0];
 
-const LOGIN_TIMEOUT = 5 * 60 * 1000;
+// To mitigate the following issue: https://github.com/DrWarpMan/steam-hour-booster/issues/9
+const LOGIN_TIMEOUT = 10 * 60 * 1000;
 
 export class Bot {
 	#username: string;
